c871c10344332b2831dbe357f963f94b4221227a
Adam Stevenson <adamstevenson121@gmail.com>
2013-12-17 16:50:56 -0500

    implement loadline

--- main.js
+++ main.js
@@ -14,24 +14,28 @@ var apple = new Apple('macintosh');
 apple.color = "reddish";
 alert(apple.getInfo());*/
 
-function Variable() {
+var title = "";
+var runfor = 0;
+
+/*
+function variable() {
     this.start = 0;
     this.stop = 0;
     this.every = 0;
-    this.function = "";
-    this.
-}
+    this.func = "";
+    this.at = 0;
+    this.update = {};
+    this.export = false;
+    this.init = 0;
+}*/
 
+var variables = {};
 
 window.onload = function() {}
 
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Year', 'Sales', 'Expenses'],
-    ['2004',  1000,      400],
-    ['2005',  1170,      460],
-    ['2006',  660,       1120],
-    ['2007',  1030,      540]
+    ['Year', 'Sales', 'Expenses'],['2004',  1000,      400],['2005',  1170,      460]
   ]);
 
   var options = {};
@@ -45,17 +49,27 @@ function clearProgram() {
   
 }
 
-function loadLine() {
-
-
+function loadLine(line) {
+    var primary = line.split(" is ");
+    console.log(primary);
+    if(primary.length == 1) {
+        var parameters = primary[0].split(" ");
+        if(parameters[0] == "title")
+            title = parameters[1];
+        if(parameters[0] == "runfor")
+            runfor = parameters[1];
+    }
 }
 
 function loadProgram() {
     var prog = $('#code-box').val();
     var lines = prog.split("\n");
     lines = lines.filter(function(n){return n});
-    $('#log-box').val(lines);
-
+    for(var l = 0; l < lines.length; l++) {
+        loadLine(lines[l]);
+    }
+    console.log("title is " + title);
+    console.log("run for " + runfor);
 
 }
 
